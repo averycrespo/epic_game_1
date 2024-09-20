39031de2fe5ffc2a708088fabc028942a1d5c7ee
mcurrie99 <45800774+mcurrie99@users.noreply.github.com>
2024-09-20 00:18:22 -0400

    Testing git CLI

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,11 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
 
+    print_something();
+
     return Ok(());
+}
+
+fn print_something() {
+    println!("Hello Avery!");
 }

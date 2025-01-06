cb9cc8cf3d2dffb6e8fd2599610ba8f4fbfd0c85
unknown <avery.crespo@gmail.com>
2025-01-06 11:19:17 -0500

    initial install for DefaultPlugins

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,60 @@
 use std::error::Error;
+use bevy::prelude::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
-
-    print_something();
-
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(PeoplePlugin)
+        .run();
+    
     return Ok(());
 }
 
-fn print_something() {
-    println!("Hello Avery!");
+pub struct PeoplePlugin;
+
+impl Plugin for PeoplePlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_systems(Startup, setup,)
+            ;
+            // .add_systems(Update, print_names)
+            // .add_systems(Update, normal_system);
+    }
+}
+
+pub fn setup(mut commands: Commands) {
+    commands.spawn(
+        (Person {
+            name: "Player".to_string(),
+        }, 
+        Employed {
+            job: Job::Police,
+        })
+    );
+}
+
+pub fn print_names(person_query: Query<&Person>) {
+    for person in person_query.iter() {
+        println!("Name: {}", person.name);
+    }
+}
+
+pub fn normal_system() {
+    println!("Worked");
+}
+
+// Required for bevy
+#[derive(Component)]
+pub struct Person{
+    pub name: String,
+}
+
+#[derive(Component)]
+pub struct Employed {
+    pub job: Job
+}
+
+pub enum Job{
+    Police, 
+    Firefighter,
 }
